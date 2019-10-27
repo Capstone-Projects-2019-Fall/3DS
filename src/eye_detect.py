d83de83c39ce584bc80da794db5cd8b2bf825c59
Matthew Vanderveer <tug52913@temple.edu>
2019-10-27 18:02:41 -0400

    full-screen view

--- src/eye_detect.py
+++ src/eye_detect.py
@@ -5,54 +5,58 @@ face_cascPath = 'C:/Users/mattv/Anaconda3/Lib/site-packages/cv2/data/haarcascade
 eye_cascPath= 'C:/Users/mattv/Anaconda3/Lib/site-packages/cv2/data/haarcascade_eye_tree_eyeglasses.xml'  #face detect model
 faceCascade = cv2.CascadeClassifier(face_cascPath)
 eyeCascade = cv2.CascadeClassifier(eye_cascPath)
-min_size = 20
+min_size = 25
 
 def findEyes(faceCascade, eyeCascade, min_size):
     cap = cv2.VideoCapture(0)
     while 1:
         ret, img = cap.read()
 
-        if ret:
-            frame = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            # Detect faces in the image
-            faces = faceCascade.detectMultiScale(
-                frame,
+        frame = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        # Detect faces in the image
+        faces = faceCascade.detectMultiScale(
+            frame,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(min_size, min_size),
+            # flags = cv2.CV_HAAR_SCALE_IMAGE
+        )
+        # print("Found {0} faces!".format(len(faces)))
+        if len(faces) == 0:
+            print(0)
+        elif len(faces) > 0:
+            # Draw a rectangle around the faces
+            for (x, y, w, h) in faces:
+                #cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+            #frame_tmp = img[faces[0][1]:faces[0][1] + faces[0][3], faces[0][0]:faces[0][0] + faces[0][2]:1, :]
+            #frame = frame[faces[0][1]:faces[0][1] + faces[0][3], faces[0][0]:faces[0][0] + faces[0][2]:1]
+                cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0), 3)
+            roi=img[y:y+h, x:x+w]
+            roi_gray=frame[y:y+h, x:x+w]
+            eyes = eyeCascade.detectMultiScale(
+                roi_gray,
                 scaleFactor=1.1,
                 minNeighbors=5,
                 minSize=(min_size, min_size),
                 # flags = cv2.CV_HAAR_SCALE_IMAGE
             )
-            # print("Found {0} faces!".format(len(faces)))
-            if len(faces) == 0:
+            for (ex, ey, ew, eh) in eyes:
+                cv2.rectangle(roi, (ex,ey), (ex+ew, ey+eh), (0,255,0), 2)
+            if len(eyes) == 0:
                 print(0)
-            elif len(faces) > 0:
-                # Draw a rectangle around the faces
-                for (x, y, w, h) in faces:
-                    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-                frame_tmp = img[faces[0][1]:faces[0][1] + faces[0][3], faces[0][0]:faces[0][0] + faces[0][2]:1, :]
-                frame = frame[faces[0][1]:faces[0][1] + faces[0][3], faces[0][0]:faces[0][0] + faces[0][2]:1]
-                eyes = eyeCascade.detectMultiScale(
-                    frame,
-                    scaleFactor=1.1,
-                    minNeighbors=5,
-                    minSize=(min_size, min_size),
-                    # flags = cv2.CV_HAAR_SCALE_IMAGE
-                )
-                if len(eyes) == 0:
-                    print(0)
+            else:
+                print(1)
 
-                else:
-                    print(1)
+            #frame_tmp = cv2.resize(frame_tmp, (400, 400), interpolation=cv2.INTER_LINEAR)
+        cv2.imshow('Face Recognition', img)
 
-                frame_tmp = cv2.resize(frame_tmp, (400, 400), interpolation=cv2.INTER_LINEAR)
-                cv2.imshow('Face Recognition', frame_tmp)
+        waitkey = cv2.waitKey(1)
 
-            waitkey = cv2.waitKey(1)
-
-            if waitkey == ord('q') or waitkey == ord('Q'):
-                cv2.destroyAllWindows()
-                break
+        if waitkey == ord('q') or waitkey == ord('Q'):
+            cv2.destroyAllWindows()
+            break
 
 def main():
     findEyes(faceCascade, eyeCascade, min_size)
