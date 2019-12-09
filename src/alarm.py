f18db000e42a6e0f1d1a6265890900e2fc0b47ea
Juwan Jordan <tug06350@temple.edu>
2019-12-09 15:52:10 -0500

    Removed redundant modules

--- src/alarm.py
+++
@@ -1,9 +0,0 @@
-# Module Purpose: Manages the notification system to alert the distracted driver to return their attention to the road.
-
-# Purpose: Triggers the driver notification sound.
-# Pre-conditions: Driver is distracted
-# Post-conditions: notification sound has gone off.
-# Parameters: driverIsDistracted
-# Return type: int return code.
-def notifyDriver():
-    pass
