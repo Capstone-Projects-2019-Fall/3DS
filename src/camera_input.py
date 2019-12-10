51c1115b58c58e90730bce8d24ad9d4bbcaa5fbe
Juwan Jordan <tug06350@temple.edu>
2019-12-09 20:58:38 -0500

    Removed redundant modules

--- src/camera_input.py
+++
@@ -1,9 +0,0 @@
-# Module Purpose: Takes image of driver to be passed to the image processing module.
-
-# Purpose: Takes image of driver and stores it locally for further processing.
-# Pre-conditions: Car is moving (tracked velocity is above specified threshold.)
-# Post-conditions: None.
-# Parameters: None.
-# Return value: image file of driver (in whatever form the raspberry pi camera API allows.)
-def getImage():
-    pass
